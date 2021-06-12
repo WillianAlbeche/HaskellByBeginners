471bc9420cf0313fff10f5f1561a5fc89034ceab
WillianAlbeche <willianalbeche@gmail.com>
2021-06-12 18:08:57 -0300

    Update A1.hs

--- Haskell/A1.hs
+++ Haskell/A1.hs
@@ -1,3 +1,7 @@
 module A1 where
 
-adicionaDois x = x + 2
+-- 1) Defina uma função recursiva insere :: Int -> [Int] -> [Int] 
+-- que insere um inteiro na posição correta em uma lista de inteiros já ordenada. 
+-- Mostre o passo-a-passo da aplicação de insere 3 [1,2,4,5]
+
+insere :: Int -> [Int] -> [Int] 
